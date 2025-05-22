184343c62649fc1934031677df3ceccfb1653518
TymeoGuedel <t.guedel@cci.formation.nc>
2025-05-22 14:52:18 +1100

    Corrige CORS : passage à HandleCors natif Laravel

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['api/*', 'login'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:5173', 'https://gitepim.netlify.app'],
+    'allowed_origins' => ['http://localhost:5173', 'https://elaborate-zabaione-974fd0.netlify.app/'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
