8e65438740376282a788716c56a92545f76e3df5
TymeoGuedel <t.guedel@cci.formation.nc>
2025-05-22 16:06:49 +1100

    Fix CORS with native Laravel middleware

--- config/cors.php
+++ config/cors.php
@@ -1,12 +1,19 @@
-<?php
-
 return [
-    'paths' => ['api/*', 'login'],
+
+    'paths' => ['api/*', 'login', 'logout', '*'],
+
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:5173', 'https://elaborate-zabaione-974fd0.netlify.app/'],
+
+    'allowed_origins' => ['http://localhost:5173'],
+
     'allowed_origins_patterns' => [],
+
     'allowed_headers' => ['*'],
+
     'exposed_headers' => [],
+
     'max_age' => 0,
+
     'supports_credentials' => false,
+
 ];
